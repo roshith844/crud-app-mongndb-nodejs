2648d599c0c5fcd7e535d89e7199a608975ada0c
Roshith P <roshith844@gmail.com>
2022-11-06 20:47:08 +0530

    Clean Code : Add Comments and Format

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
-const express = require("express")
-const mongoose = require("mongoose")
-const session = require("express-session")
+const express = require("express");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const app = express();
+const PORT = process.env.PORT || 4000;
 
-const app =  express()
-const PORT =  process.env.PORT || 4000
-mongoose.connect("mongodb://localhost:27017")
-const db = mongoose.connection
-db.on("error", (error)=> console.log(error))
-db.once("open", ()=> console.log('connected to database'))
-app.get('/', (req, res)=>{
-     res.send("working")
-})
-app.listen(PORT, ()=>{
-     console.log("server started")
-})
+// Connect MongoDB Database
+mongoose.connect("mongodb://localhost:27017");
+const db = mongoose.connection;
+db.on("error", (error) => console.log(error));
+db.once("open", () => console.log("connected to database"));
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("working");
+});
+
+// Server
+app.listen(PORT, () => {
+  console.log("server started");
+});
