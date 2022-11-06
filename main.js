cf71959a13173c454f8028334c66391299bb90bc
Roshith P <roshith844@gmail.com>
2022-11-06 21:24:45 +0530

    Add Middlewares
    Convert to json
    initiate session

--- main.js
+++ main.js
@@ -4,6 +4,24 @@ const session = require("express-session");
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+// Middlewares
+router.use(express.urlencoded({ extended: true }));
+router.use(express.json());
+
+router.use(
+  session({
+    secret: "secret",
+    saveUninitialized: true,
+    resave: false,
+  })
+);
+
+app.use((req, res, next) => {
+  res.locals.messege = req.session.messege;
+  delete req.session.messege;
+  next();
+});
+
 // Connect MongoDB Database
 mongoose.connect("mongodb://localhost:27017");
 const db = mongoose.connection;
