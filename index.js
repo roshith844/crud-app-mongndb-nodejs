4baebaa4fe75b398b9d846747c18f340b6cf9d1b
Roshith P <roshith844@gmail.com>
2022-11-07 17:51:23 +0530

    Add : Express handlebars as View engine

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const connection = require("./model");
 const express = require("express");
 const app = express();
 const path = require("path");
-const hbs = require("express-handlebars");
+const exphbs = require("express-handlebars");
 const bodyparser = require("body-parser");
 
 app.use(
@@ -10,6 +10,19 @@ app.use(
     extended: true,
   })
 );
+
+app.set("views", path.join(__dirname, "/views/")); // setting views
+
+app.engine(
+  "hbs",
+  exphbs.engine({
+    extname: "hbs",
+    defaultLayout: "mainLayout",
+    layoutsDir: __dirname + "views/layouts",
+  })
+);
+
+app.set("view engine", "hbs");
 
 app.get("/", (req, res) => {
   res.send("home page");
