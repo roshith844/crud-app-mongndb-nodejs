441601e5371fdf7b55be5eac04d5997b4ab544bb
Roshith P <roshith844@gmail.com>
2022-11-07 09:39:06 +0530

    Fix ( Router ) : Router Not working Remove unwanted middlewares Fix typos Move router middlewares to routes folder From 'main.js'

--- main.js
+++ main.js
@@ -1,32 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const session = require("express-session");
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-// Middlewares
-router.use(express.urlencoded({ extended: true }));
-router.use(express.json());
-
-router.use(
-  session({
-    secret: "secret",
-    saveUninitialized: true,
-    resave: false,
-  })
-);
-
-app.use((req, res, next) => {
-  res.locals.messege = req.session.messege;
-  delete req.session.messege;
-  next();
-});
-
 // Router prefix
-app.use('', require('./routes/routes'))
+app.use("", require("./routes/routes"));
 
 // Sets View Engine
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
 
 // Connects MongoDB Database
 mongoose.connect("mongodb://localhost:27017");
--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,21 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
+const session = require("express-session");
 
-router.ger('/users', (req, res)=>{
-     res.send('All users')
-})
+// Middlewares
+router.use(express.urlencoded({ extended: true }));
+router.use(express.json());
 
-module.exports = router
+router.use(
+  session({
+    secret: "secret",
+    saveUninitialized: true,
+    resave: false,
+  })
+);
+
+router.get("/users", (req, res) => {
+  res.send("All users");
+});
+
+module.exports = router;
